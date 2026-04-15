e9d86ea65ae3eedc50dde73e10e32048bce4e398
FyrooCode <fyroocode@gmail.com>
2026-04-15 15:34:08 +0700

    feat: setup boilerplate

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "test",
+			"status":  "Active",
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
